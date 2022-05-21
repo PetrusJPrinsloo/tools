0f4e4db3b77b06a220df3794fe078678c2c7fed8
Petrus Prinsloo <petrus.prinsloo@gmail.com>
2022-05-21 21:46:23 +0200

    Created an echo server that repeats back over same TCP connection what it receives

--- echoserver.go
+++ echoserver.go
@@ -1,33 +1,40 @@
 package main
 
 import (
-	"fmt"
 	"io"
 	"log"
-	"os"
+	"net"
 )
 
-type FooReader struct{}
+func echo(conn net.Conn) {
+	defer func(conn net.Conn) {
+		err := conn.Close()
+		if err != nil {
+			log.Printf("Connection to a client did not close properly")
+			log.Print(err)
+		}
+	}(conn)
 
-func (fooReader *FooReader) Read(b []byte) (int, error) {
-	fmt.Print("in> ")
-	return os.Stdin.Read(b)
-}
-
-type FooWriter struct{}
-
-func (fooWriter *FooWriter) Write(b []byte) (int, error) {
-	fmt.Print("out> ")
-	return os.Stdout.Write(b)
+	if _, err := io.Copy(conn, conn); err != nil {
+		log.Fatalln("Unable to read/write data")
+	}
 }
 
 func main() {
-	var (
-		reader FooReader
-		writer FooWriter
-	)
+	listener, err := net.Listen("tcp", ":20080")
+	if err != nil {
+		log.Fatalln("Unable to bind to port")
+	}
 
-	if _, err := io.Copy(&writer, &reader); err != nil {
-		log.Fatalln("Unable to read/write data")
+	log.Println("listening on port 0.0.0.0:20080")
+	for {
+		conn, err := listener.Accept()
+
+		log.Println("Received connection")
+		if err != nil {
+			log.Fatalln("Unable to accept connection")
+		}
+
+		go echo(conn)
 	}
 }
